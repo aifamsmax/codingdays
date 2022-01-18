74b62922e4fe324fdcd6f3a4fdd9306a56308ceb
aifamsmax <aifamsmax@gmail.com>
2022-01-18 12:50:46 +0530

    18 Jan 2022

---
+++ day18.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+        
+        int sum =0;
+        int lSum = 0;
+        
+        for(int i = 0; i<n; i++){
+            sum+= a[i];
+        }
+        for(int j = 0; j < n; j++){
+            if(lSum == sum - a[j]){
+                return j + 1;
+            }
+            sum-=a[j];
+            lSum+= a[j];
+        }
+        return -1;
+    }
+
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
